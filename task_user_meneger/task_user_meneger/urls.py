e47b6ccff0f483e644547aefda309d385e031cda
alexandr_fin_calc <aleksandraseev14212@gmail.com>
2024-11-18 04:29:08 -0800

    + users

--- task_user_meneger/task_user_meneger/urls.py
+++ task_user_meneger/task_user_meneger/urls.py
@@ -14,16 +14,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from os.path import basename
+
 from django.contrib import admin
 from django.urls import path, include
 
 from tasks.views import TaskViewSet
+from users.views import UserViewSet
 from rest_framework import routers
 
-router = routers.SimpleRouter()
-router.register(r'tasks', TaskViewSet)
+router1 = routers.SimpleRouter()
+router1.register(r'tasks', TaskViewSet, basename='tasks')
+
+router2 = routers.SimpleRouter()
+router2.register(r'users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include(router.urls)),  # http://127.0.0.1:8000/api/v1/tasks/
+    path('api/v1/', include(router1.urls)),  # http://127.0.0.1:8000/api/v1/tasks/
+    path('api/v2/', include(router2.urls)),  # http://127.0.0.1:8000/api/v2/users/
+
 ]
