12d942b1eb7a926ec3e24d897d9ed47e956809a5
alexandr <aseevalexandr@bk.ru>
2024-11-30 10:45:39 +0300

    work version

--- task_user_meneger/users/serializers.py
+++ task_user_meneger/users/serializers.py
@@ -8,3 +8,9 @@ class UserSerializers(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username', 'email', 'password']
 
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        user.is_active = True  # Убедитесь, что пользователь активирован
+        user.save()
+        return user
+
--- task_user_meneger/users/tests.py
+++ task_user_meneger/users/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth import get_user_model
+from rest_framework.reverse import reverse
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your tests here.
+User = get_user_model()
+
+
+class UserViewSetTest(APITestCase):
+
+    def setUp(self):
+        """
+        Этот метод выполняется перед каждым тестом.
+        Мы создаем тестового пользователя, чтобы использовать его в тестах.
+        """
+        self.test_user = User.objects.create_user(username='testuser', email='testuser@example.com',
+                                                  password='password123')
+        self.test_user.is_active = True
+        self.test_user.save()
+
+        self.url = reverse('api/v2/users/')  # Получаем URL для списка пользователей (UserViewSet)
+
+    def test_create_user(self):
+        """
+        Проверяем, что можно создать нового пользователя.
+        """
+        data = {
+            'username': 'newuser',
+            'email': 'newuser@example.com',
+            'password': 'password123'
+        }
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
