e3920815dfd1c637d153ee8f5b15e72d12c47d41
alexandr <aseevalexandr@bk.ru>
2024-12-04 10:35:51 +0300

    + reviev

--- task_user_meneger/users/models.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.admin.utils import model_ngettext
-from django.db import models
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-"""
-Создаём модель User из встроенной в Django модели
-"""
