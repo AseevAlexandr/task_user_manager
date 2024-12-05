ea74c3a3cd18070678e4713787981002118ca29b
alexandr <aseevalexandr@bk.ru>
2024-12-05 12:32:17 +0300

    correction

--- users/models.py
+++ users/models.py
@@ -2,7 +2,9 @@ from django.contrib.admin.utils import model_ngettext
 from django.db import models
 from django.contrib.auth import get_user_model
 
+"""
+Создаём переменную User, используя встроенную в Django модель get_user_model() кастомизируя её под задачи своего проекта
+"""
+
 User = get_user_model()
-"""
-Создаём модель User из встроенной в Django модели
-"""
+
